aa595d7fe719e4db672a71d9548c6fb217bc19ce
mihir.sharma@unc.edu <mihir.sharma@unc.edu>
2024-09-29 00:32:14 -0400

    FINAL WORKING COMMIT

--- webapp/frontend/src/app/page.tsx
+++ webapp/frontend/src/app/page.tsx
@@ -30,6 +30,7 @@ export default function LandingPage() {
               <div className="flex flex-col items-center space-y-4 text-center">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+
                     <TypeAnimation
                       sequence={[
                         // Same substring at the start will only be typed out once, initially
